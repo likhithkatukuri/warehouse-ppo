ac037c1d9787b17da2acc1badaef20bd877c94ea
likhithkatukuri <likhithkatukuri8@gmail.com>
2026-07-22 12:37:35 +0530

    Add dataset-based warehouse environment

--- trian_ppo.py
+++ trian_ppo.py
@@ -1,45 +1,134 @@
-from utils.lidar_loader import LiDARLoader
-from utils.occupancy_grid import OccupancyGridGenerator
-from utils.start_goal_generetor import StartGoalGenerator
+import os
 
-from env.warehouse_env import WarehouseEnvironment
+from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.callbacks import (
+    CheckpointCallback,
+    EvalCallback,
+)
 
-from planners.ppo import PPOTrainer
+from environment.warehouse_dataset_env import WarehouseDatasetEnvironment
 
-############################################################
-
-loader = LiDARLoader(r"C:\Users\FOX TROT\Downloads\warehouse_data\Release_v1.0.0-001\Dataset\bin")
-
-points = loader.load_scan("000000.bin")
-
-############################################################
-
-generator = OccupancyGridGenerator()
-
-grid = generator.generate(points)
 
 ############################################################
-
-sg = StartGoalGenerator(grid)
-
-start, goal = sg.generate()
-
+# Create Environment
 ############################################################
 
-env = WarehouseEnvironment(
+def make_env():
 
-    grid,
+    env = WarehouseDatasetEnvironment()
 
-    start,
+    env = Monitor(env)
 
-    goal
+    return env
+
+
+train_env = DummyVecEnv([make_env])
+
+eval_env = DummyVecEnv([make_env])
+
+
+############################################################
+# Directories
+############################################################
+
+os.makedirs("models", exist_ok=True)
+
+os.makedirs("logs", exist_ok=True)
+
+
+############################################################
+# Callbacks
+############################################################
+
+checkpoint_callback = CheckpointCallback(
+
+    save_freq=10000,
+
+    save_path="./models/",
+
+    name_prefix="ppo_warehouse"
 
 )
 
+eval_callback = EvalCallback(
+
+    eval_env,
+
+    best_model_save_path="./models/best_model/",
+
+    log_path="./logs/",
+
+    eval_freq=10000,
+
+    deterministic=True,
+
+    render=False
+
+)
+
+
+############################################################
+# PPO Model
 ############################################################
 
-trainer = PPOTrainer(env)
+model = PPO(
 
-trainer.train(
-    total_timesteps=50000
+    policy="CnnPolicy",
+
+    env=train_env,
+
+    learning_rate=3e-4,
+
+    n_steps=2048,
+
+    batch_size=64,
+
+    n_epochs=10,
+
+    gamma=0.99,
+
+    gae_lambda=0.95,
+
+    clip_range=0.2,
+
+    ent_coef=0.01,
+
+    vf_coef=0.5,
+
+    max_grad_norm=0.5,
+
+    tensorboard_log="./logs/",
+
+    verbose=1,
+
+    device="auto"
+
 )
+
+
+############################################################
+# Train
+############################################################
+
+TOTAL_TIMESTEPS = 500000
+
+model.learn(
+
+    total_timesteps=TOTAL_TIMESTEPS,
+
+    callback=[checkpoint_callback, eval_callback],
+
+    progress_bar=True
+
+)
+
+
+############################################################
+# Save Final Model
+############################################################
+
+model.save("models/ppo_warehouse_final")
+
+print("\nTraining Complete!")
